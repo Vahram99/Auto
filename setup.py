78febceb726281e8fac20a8cf4d120baa6201449
Vahram Babajanyan <vahram.babadjanyan@gmail.com>
2022-12-19 11:45:18 +0400

    closes #9, add reqs to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
 import os
+from pip.req import parse_requirements
 
+def dirname(fname):
+    return os.path.join(os.path.dirname(__file__), fname)
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(dirname(fname)).read()
 
+install_reqs = parse_requirements(dirname('requirements.txt'))
+reqs = [str(ir.req) for ir in install_reqs]
 
 __version__ = "0.0.1"
 
@@ -22,4 +27,5 @@ setup(
     description = 'Automated hyperparameter optimization',
     long_description = read('README.md'),
     keywords = "machine-learning automated hyperparameter optimization",
-)
+    install_requires = reqs
+) 
